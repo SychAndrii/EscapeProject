c3fe63d5f63e650d6c1fb9f45ddbdc49a95d4625
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-09-17 20:46:15 -0400

    Added display of time range and duration for the tasks

--- Task/TaskEntity.cs
+++ Task/TaskEntity.cs
@@ -5,11 +5,13 @@ namespace EscapeProject.Task
     public class TaskEntity
     {
         public string name { get; set; } = string.Empty;
+
         [JsonConverter(typeof(StrictDateTimeConverter))]
         public DateTime? from
         {
             get; set;
         }
+
         [JsonConverter(typeof(StrictDateTimeConverter))]
         public DateTime? until
         {
