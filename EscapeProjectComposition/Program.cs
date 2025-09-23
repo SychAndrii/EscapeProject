32a1fe9f61f1b3c7731b69173973c2b6e79e6a04
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-09-23 15:48:26 -0400

    Finished fixing windows installer

--- EscapeProjectComposition/Program.cs
+++ EscapeProjectComposition/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Runtime.InteropServices;
 using EscapeProjectComposition;
+using EscapeProjectComposition.GenerateTaskPlan.Factories;
+using EscapeProjectComposition.GenerateTaskPlan.FactoryProviders;
 using EscapeProjectPresentationCLI.Commands.GenerateTaskPlan;
+using EscapeProjectPresentationCLI.UseCaseFactoryProviders.GenerateTaskPlan;
 using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console.Cli;
 
@@ -8,11 +11,27 @@ internal class Program
 {
     private static async Task<int> Main(string[] args)
     {
-        var registrar = new TypeRegistrar(new ServiceCollection());
+        var services = new ServiceCollection();
+
+        services.AddTransient<GenerateExcelTaskPlanUseCaseFactory>();
+        services.AddTransient<GeneratePDFTaskPlanUseCaseFactory>();
+
+        // Register providers
+        services.AddTransient<IGenerateExcelTaskPlanUseCaseFactoryProvider, GenerateExcelTaskPlanUseCaseFactoryProvider>();
+        services.AddTransient<IGeneratePDFTaskPlanUseCaseFactoryProvider, GeneratePDFTaskPlanUseCaseFactoryProvider>();
+
+        // Register commands
+        services.AddTransient<GenerateExcelTaskPlanCommand>();
+        services.AddTransient<GeneratePDFTaskPlanCommand>();
+
+        // Wrap in Spectre registrar
+        var registrar = new TypeRegistrar(services);
         var app = new CommandApp(registrar);
 
         app.Configure(config =>
         {
+            config.PropagateExceptions();
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 config.SetApplicationName("escape-run.bat");
@@ -23,7 +42,8 @@ internal class Program
             }
 
             config.AddCommand<GenerateExcelTaskPlanCommand>("generate-excel")
-                   .WithExample(new[] { "generate-excel", "-h" });
+                  .WithExample(new[] { "generate-excel", "-h" });
+
             config.AddCommand<GeneratePDFTaskPlanCommand>("generate-pdf")
                   .WithExample(new[] { "generate-pdf", "-h" });
         });
