30915bd7977dfb5a362aab1b2ae92ff046b435fa
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-09-23 13:39:28 -0400

    Added examples for the help message

--- EscapeProjectComposition/Program.cs
+++ EscapeProjectComposition/Program.cs
@@ -1,7 +1,6 @@
-﻿using EscapeProjectComposition;
-using EscapeProjectComposition.GenerateTaskPlan.FactoryProviders;
+﻿using System.Runtime.InteropServices;
+using EscapeProjectComposition;
 using EscapeProjectPresentationCLI.Commands.GenerateTaskPlan;
-using EscapeProjectPresentationCLI.UseCaseFactoryProviders.GenerateTaskPlan;
 using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console.Cli;
 
@@ -9,24 +8,24 @@ internal class Program
 {
     private static async Task<int> Main(string[] args)
     {
-        var services = new ServiceCollection();
-
-        services.AddTransient<IGenerateExcelTaskPlanUseCaseFactoryProvider, GenerateExcelTaskPlanUseCaseFactoryProvider>();
-        services.AddTransient<IGeneratePDFTaskPlanUseCaseFactoryProvider, GeneratePDFTaskPlanUseCaseFactoryProvider>();
-
-        services.AddTransient<GenerateExcelTaskPlanCommand>();
-        services.AddTransient<GeneratePDFTaskPlanCommand>();
-
-        var registrar = new TypeRegistrar(services);
+        var registrar = new TypeRegistrar(new ServiceCollection());
         var app = new CommandApp(registrar);
 
         app.Configure(config =>
         {
-            config.AddCommand<GenerateExcelTaskPlanCommand>("generate-excel")
-                .WithDescription("Generate a task plan in Excel format");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                config.SetApplicationName("escape-run.bat");
+            }
+            else
+            {
+                config.SetApplicationName("escape-run.sh");
+            }
 
+            config.AddCommand<GenerateExcelTaskPlanCommand>("generate-excel")
+                   .WithExample(new[] { "generate-excel", "-h" });
             config.AddCommand<GeneratePDFTaskPlanCommand>("generate-pdf")
-                .WithDescription("Generate a task plan in PDF format");
+                  .WithExample(new[] { "generate-pdf", "-h" });
         });
 
         return await app.RunAsync(args);
