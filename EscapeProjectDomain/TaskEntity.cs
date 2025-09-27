8b161dfd258560538f5dc5e51849a9d159afd9b8
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-09-27 14:00:35 -0400

    Refactored from and until logic to be computed from subtasks if not set

--- EscapeProjectDomain/TaskEntity.cs
+++ EscapeProjectDomain/TaskEntity.cs
@@ -37,7 +37,31 @@ namespace EscapeProjectDomain
 
         public DateTime? From
         {
-            get => from;
+            get
+            {
+                DateTime? currentFrom = from;
+                if (currentFrom != null)
+                {
+                    return currentFrom;
+                }
+
+                if (Subtasks != null)
+                {
+                    DateTime? currentUntil = Until;
+                    DateTime maxUntil = currentUntil ?? DateTime.MaxValue;
+
+                    var tasksWithKnownFrom = Subtasks.Where(t => t.From != null && t.From < maxUntil);
+                    if (tasksWithKnownFrom.Count() == 0)
+                    {
+                        return null;
+                    }
+
+                    var minFromInSubtasks = tasksWithKnownFrom.Min(t => t.From);
+                    return minFromInSubtasks;
+                }
+
+                return null;
+            }
             set
             {
                 if (Until != null && value != null && value >= Until)
@@ -50,7 +74,31 @@ namespace EscapeProjectDomain
 
         public DateTime? Until
         {
-            get => until;
+            get
+            {
+                DateTime? currentUntil = until;
+                if (currentUntil != null)
+                {
+                    return currentUntil;
+                }
+
+                if (Subtasks != null)
+                {
+                    DateTime? currentFrom = From;
+                    DateTime minFrom = currentFrom ?? DateTime.MinValue;
+
+                    var tasksWithKnownUntil = Subtasks.Where(t => t.Until != null && t.Until > minFrom);
+                    if (tasksWithKnownUntil.Count() == 0)
+                    {
+                        return null;
+                    }
+
+                    var maxUntilInSubtasks = tasksWithKnownUntil.Max(t => t.Until);
+                    return maxUntilInSubtasks;
+                }
+
+                return null;
+            }
             set
             {
                 if (From != null && value != null && value <= From)
@@ -61,18 +109,7 @@ namespace EscapeProjectDomain
             }
         }
 
-        public TimeSpan? Duration
-        {
-            get
-            {
-                DateTime? effectiveFrom = From ?? Subtasks?.Where(s => s.From != null).Min(s => s.From);
-                DateTime? effectiveUntil = Until ?? Subtasks?.Where(s => s.Until != null).Max(s => s.Until);
-
-                return effectiveFrom != null && effectiveUntil != null
-                    ? effectiveUntil.Value - effectiveFrom.Value
-                    : null;
-            }
-        }
+        public TimeSpan? Duration => Until.HasValue && From.HasValue ? Until.Value - From.Value : null;
 
         protected override IEnumerable<object?> GetEqualityComponents()
         {
